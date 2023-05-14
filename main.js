3d62cb0669de5212bcfe1142ce803ea0aec74eaf
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2023-05-14 15:51:05 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Problem 1
+
+const firstName = "Levan";
+const lastName = "Mtchedlishvili";
+const gender = "Male";
+const age = 22;
+
+// Problem 2
+
+const numReminder = 101 % 2;
+console.log(numReminder);
